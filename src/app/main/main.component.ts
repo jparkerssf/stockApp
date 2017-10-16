1af52fe6e9bb0532b2fb9744343c03a219c97076
jparkerssf <jparker@softstackfactory.org>
2017-10-16 04:22:32 +0000

    added the watchlist functionality and data checking

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,6 +5,7 @@ import { OnInit } from '@angular/core';
 import Chart from 'chart.js';
 import { ViewChild } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
+import {MdCardModule} from '@angular/material';
 
 @Component({
   selector: 'app-main',
@@ -25,30 +26,56 @@ export class MainComponent  {
     responsive: true
   
   };
+  todaysData= {
+    open:" ",
+    high: " ",
+    low:" ",
+    close:" "
+    
+  };
+  smaArray:any = [];
   lastRefreshed:string;
   timeZone:string;
   stockSymbol:string;
+  smaRefreshed:string
+  closingData:any = []
   news:any = [];
+  
+  
+  watchList:Array<any> = [];
  @ViewChild(BaseChartDirective)
     public chart: BaseChartDirective;
-    
+    //sample data 
   ngAfterViewInit() {
        ///Here we call the ny times api to get the top news article
-    this.stockService.getNews()
-      .subscribe (
-              (data) => { 
-                      console.log(data, "We got some data!");
-                      this.news = data.results;
-                      console.log(this.news,"here is the news!");
-                    
+    // this.stockService.getSma()
+    //   .subscribe (
+    //           (data) => { 
+    //                   console.log(data, "We got some data!");
+    //                   this.smaArray = data;
+    //                   console.log(this.smaArray,"this is the sma");
+    //                   console.log(" the last refreshed time", data["Meta Data"]["3: Last Refreshed"])
+    //                   this.smaRefreshed = data["Meta Data"]["3: Last Refreshed"]
+    //                   let smaRefreshDate = data["Meta Data"]["3: Last Refreshed"]
+    //                   let smaObject = data["Technical Analyses: SMA"]
+    //                   // for(var key in smaObject){
+    //                   //   if(key == smaRefreshDate){
+    //                   //       console.log("key", key)
+    //                   //     let todaysData = smaObject[key].SMA
+    //                   //   }
+                         
+    //                   // }
+                       
+    //                 //console.log("todaysData", todaysData);
+    //               // return todaysData;   
                       
-              },
-              (error) => {
-                console.log(error);
+    //           },
+    //           (error) => {
+    //             console.log(error);
                
-              }
+    //           }
             
-            )
+    //         )
       
       
       
@@ -57,7 +84,8 @@ export class MainComponent  {
     this.stockService.getStock()
     .subscribe(
         (data) => {
-     
+          
+          
             console.log(this.lineChartData[0].data,"check graph dat" )
             console.log("RAW DATA", data);
             this.lastRefreshed = data["Meta Data"]["3. Last Refreshed"];
@@ -66,21 +94,37 @@ export class MainComponent  {
             console.log("monthly time series", data["Monthly Time Series"])
             let dataObject = data["Time Series (15min)"]
             
+            
                 for (var key in dataObject) {
                     this.lineChartLabels.push(key);
+                    
+                    
 
+                }
+                
+                 for (var key in dataObject) {
+                    this.closingData.push(dataObject[key]);
                 }
                 console.log(dataObject,"dataObject")
                 for(var key in dataObject){
                   if(dataObject.hasOwnProperty(key)){
+                   
+                    
                     let stockObject = dataObject[key];
                          
                         this.lineChartData[0].data.push(stockObject["2. high"]);
+                    
                   }
                 }
                 console.log(this.lineChartData,"line chart data");
-                
-               
+                console.log(this.closingData,"closing data");
+          
+              this.todaysData.open =  this.closingData[0]["1. open"] 
+              this.todaysData.high =  this.closingData[0]["2. high"] 
+              this.todaysData.low =  this.closingData[0]["3. low"] 
+              this.todaysData.close =  this.closingData[0]["4. close"] 
+              console.log(this.todaysData,"todays data")
+
         },
         (error)=>{
            
@@ -154,7 +198,17 @@ export class MainComponent  {
   
   ///the result from searching the stock api
   stockQuery(query){
-                console.log(this.lineChartData[0].data,"check graph data" )
+               this.todaysData= {
+                    open:" ",
+                    high: " ",
+                    low:" ",
+                    close:" "
+              
+            };
+    
+       this.closingData = []
+    
+         console.log(this.lineChartData[0].data,"check graph data" )
           this.lineChartData[0].data = [];
                       this.lineChartLabels = []
 
@@ -171,19 +225,29 @@ export class MainComponent  {
             
                 for (var key in dataObject) {
                     this.lineChartLabels.push(key);
+                    
 
+                }
+                 for (var key in dataObject) { 
+                    this.closingData.push(dataObject[key]);
                 }
                 console.log(dataObject,"dataObject")
                 for(var key in dataObject){
                   if(dataObject.hasOwnProperty(key)){
                     let stockObject = dataObject[key];
                         this.lineChartData[0].data.push(stockObject["2. high"]);
+                        
+                        
                   }
                 }
                 console.log(this.lineChartData,"line chart data");
                
-            
-            
+                 this.todaysData.open =  this.closingData[0]["1. open"] 
+                this.todaysData.high =  this.closingData[0]["2. high"] 
+                this.todaysData.low =  this.closingData[0]["3. low"] 
+                this.todaysData.close =  this.closingData[0]["4. close"] 
+                console.log(this.todaysData,"todays data")
+              
             
             
             
@@ -203,7 +267,17 @@ export class MainComponent  {
      
   }
   
+addStock(symbol){
+  console.log("function triggered to add stock");
+   if(!this.watchList.includes(symbol)){
+           this.watchList.push(symbol);
+           console.log(this.watchList,"WatchList array");
+       } else{
+            alert("Already in your favorites!")
+       }
 
+  console.log("my watch list ", this.watchList)
+}
  
   
   
--- src/app/stock-service.service.ts
+++ src/app/stock-service.service.ts
@@ -4,10 +4,14 @@ import 'rxjs/add/operator/map';
 import {Observable} from "rxjs/Observable";
 @Injectable()
 export class StockServiceService {
-   newsUrl:string =  "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=e9e5e1c20237468cbe71302a56521556";
-     dailyUrl:string = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&outputsize=full&apikey=demo"
+    newsUrl:string =  "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=e9e5e1c20237468cbe71302a56521556";
+    dailyUrl:string = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=15min&outputsize=full&apikey=demo"
     url:string = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=TA0MFSEH1N40C4ZO'
     searchUrl:string = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='
+    smaUrl :string = "https://www.alphavantage.co/query?function=SMA&symbol=MSFT&interval=15min&time_period=10&series_type=close&apikey=TA0MFSEH1N40C4ZO"
+    searchSma :string = "https://www.alphavantage.co/query?function=SMA&symbol=" 
+    searchSma2:string = "&interval=15min&time_period=10&series_type=close&apikey=TA0MFSEH1N40C4ZO"
+
 
   constructor(public http: Http) { }
   
@@ -50,6 +54,33 @@ export class StockServiceService {
       
       
   }
+  //  getSma(stockSymbol) {
+  //     return this.http.get(this.searchSma + stockSymbol + this.searchSma2)
+  //     .map(
+  //         (response:Response) => {
+          
+  //         const smaData = response.json();
+  //         return smaData;
+          
+  //     }
+  //     )
+      
+      
+  // }
+  smaSearch() {
+      return this.http.get(this.smaUrl)
+      .map(
+          (response:Response) => {
+          
+          const smaData = response.json();
+          return smaData;
+          
+      }
+      )
+      
+      
+  }
+  
   
   
  
